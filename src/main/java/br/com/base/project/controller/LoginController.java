9bba149b3d7af0cf4bed16c9f0de5cfa45d762cd
Lucas de Oliveira da Silva <lucas@inventfy.com.br>
2022-09-05 15:10:20 -0300

    update

--- src/main/java/br/com/base/project/controller/LoginController.java
+++ src/main/java/br/com/base/project/controller/LoginController.java
@@ -1,18 +1,20 @@
 package br.com.base.project.controller;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
-//	@RequestMapping("/login")
-//	public String login(@AuthenticationPrincipal User user) {
-//		if (user != null) {
-//			return "redirect:/";
-//		}
-//		return "login.html";
-//	}
+	@RequestMapping("/login")
+	public String login(@AuthenticationPrincipal User user) {
+		if (user != null) {
+			return "redirect:/";
+		}
+		return "login.html";
+	}
 	
 	@RequestMapping("/")
 	public String index() {
