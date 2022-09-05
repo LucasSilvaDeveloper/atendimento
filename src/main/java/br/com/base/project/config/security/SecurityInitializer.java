cf951ea525d44f585a826925ec6775a28e3a723b
Lucas de Oliveira da Silva <lucas@inventfy.com.br>
2022-09-05 15:08:21 -0300

    update

--- src/main/java/br/com/base/project/config/security/SecurityInitializer.java
+++ src/main/java/br/com/base/project/config/security/SecurityInitializer.java
@@ -1,7 +1,7 @@
-//package br.com.base.project.config.security;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer{
-//
-//}
+package br.com.base.project.config.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
--- src/main/java/br/com/base/project/model/Usuario.java
+++ src/main/java/br/com/base/project/model/Usuario.java
@@ -1,68 +1,68 @@
-//package br.com.base.project.model;
-//
-//import java.util.Collection;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import lombok.Data;
-//
-//@Data
-//@Entity
-//public class Usuario implements UserDetails{
-//
-//	/**
-//	 * 
-//	 */
-//	private static final long serialVersionUID = 5761296561359180938L;
-//
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.IDENTITY)
-//	private Long id;
-//	
-//	private String login;
-//	
-//	private String senha;
-//	
-//	@Override
-//	public Collection<? extends GrantedAuthority> getAuthorities() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public String getPassword() {
-//		return this.senha;
-//	}
-//
-//	@Override
-//	public String getUsername() {
-//		return this.login;
-//	}
-//
-//	@Override
-//	public boolean isAccountNonExpired() {
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean isAccountNonLocked() {
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean isCredentialsNonExpired() {
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean isEnabled() {
-//		return true;
-//	}
-//	
-//}
+package br.com.base.project.model;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Usuario implements UserDetails{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5761296561359180938L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String login;
+	
+	private String senha;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+}
